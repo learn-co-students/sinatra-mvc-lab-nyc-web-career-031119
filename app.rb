529a11ca307bdcb3ea895e98cb4eefb97b94a706
J-Agens <johntagens@gmail.com>
2019-04-02 13:48:06 -0400

    done.

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/' do
+    @platin_text = PigLatinizer.new(params[:user_text])
+    erb :result
+  end
 end
