6896071d37d2485f432c791a7ecbd24cc7f1412e
ezevin <ehzevin@gmail.com>
2019-04-02 11:00:27 -0400

    Done.

--- spec/models/piglatinizer_spec.rb
+++ spec/models/piglatinizer_spec.rb
@@ -2,6 +2,7 @@ describe 'class PigLatinizer' do
   let!(:words) { PigLatinizer.new }
 
 
+
   it 'can create a new instance of a class' do
     expect(PigLatinizer.new).to be_an_instance_of(PigLatinizer)
   end
